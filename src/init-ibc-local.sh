c28c6b4457da1fc7d2a0d877c9b2ca8973f8feb3
Justin R. Evans <jurevans@gmail.com>
2022-07-01 05:40:58 -0400

    Copy all required files to appropriate places

--- src/init-ibc-local.sh
+++ src/init-ibc-local.sh
@@ -19,8 +19,12 @@ EOF
 
 STATUS_INFO="\e[0m[\e[1;32m+\e[0m]"
 STATUS_WARN="\e[0m[\e[1;33m!\e[0m]"
-STATUS_ERROR="\e[0m[\e[1;34m*\e[0m]"
+STATUS_NOTICE="\e[0m[\e[1;34m*\e[0m]"
 STATUS_FAIL="\e[0m[\e[1;31mx\e[0m]"
+
+HERMES_CONFIG_TEMPLATE="config_template.toml"
+CHAIN_A_TEMPLATE="#{CHAIN_A_ID}"
+CHAIN_B_TEMPLATE="#{CHAIN_B_ID}"
 
 check_dependencies() {
   if ! command -v git &> /dev/null
@@ -53,7 +57,7 @@ else
   BASE_IBC_PATH=$(pwd)
 fi
 
-BASE_DIR="build"
+BUILD_DIR="$BASE_IBC_PATH/build"
 ANOMA_DIR="anoma"
 HERMES_DIR="ibc-rs"
 
@@ -105,29 +109,29 @@ HERMES_GIT_URL="$GITHUB_HTTPS_URL$HERMES_REPO"
 
 check_dependencies
 
-mkdir -p "$BASE_IBC_PATH/$BASE_DIR"
-cd "$BASE_IBC_PATH/$BASE_DIR" && printf "\n$STATUS_WARN Changed directory to $(pwd)\n"
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR" && printf "\n$STATUS_WARN Changed directory to $(pwd)\n"
 
 # Clone anoma and ibc-rs repositories
 
 # anoma
 printf "\n$STATUS_INFO Cloning $ANOMA_GIT_URL\n"
 [ ! -d $ANOMA_DIR ] && git clone $ANOMA_GIT_URL || \
-  printf "$STATUS_ERROR Directory anoma exists, skipping git clone...\n\n"
+  printf "$STATUS_NOTICE Directory anoma exists, skipping git clone...\n\n"
 
 # Hermes (ibc-rs)
 printf "$STATUS_INFO Cloning $HERMES_GIT_URL\n"
 [ ! -d $HERMES_DIR ] && git clone $HERMES_GIT_URL || \
-  printf "$STATUS_ERROR Directory ibc-rs exists, skipping git clone...\n\n"
+  printf "$STATUS_NOTICE Directory ibc-rs exists, skipping git clone...\n\n"
 
 # Install Anoma
 printf "\e$STATUS_INFO Installing Anoma\n"
-cd $ANOMA_DIR && printf "\n$STATUS_WARN Changed directory to $(pwd)\n\n" && \
+cd $BUILD_DIR/$ANOMA_DIR && printf "\n$STATUS_WARN Changed directory to $(pwd)\n\n" && \
   git checkout $ANOMA_BRANCH && make install && make build-wasm-scripts
 
 # Initialize Namada Chains
-cd $BASE_IBC_PATH/$BASE_DIR/$ANOMA_DIR && printf "$STATUS_WARN Changed directory to $(pwd)\n\n"
 
+# CHAIN A
 printf "$STATUS_INFO Initializing Chain A\n\n"
 CHAIN_A_INIT_STDOUT=$(./target/release/anomac utils init-network \
   --unsafe-dont-encrypt \
@@ -138,10 +142,14 @@ CHAIN_A_INIT_STDOUT=$(./target/release/anomac utils init-network \
   --wasm-checksums-path $WASM_CHECKSUMS_PATH)
 
 CHAIN_A_ID=$( echo "${CHAIN_A_INIT_STDOUT%?}" | grep "Derived" | sed 's/Derived chain ID: //g' )
+CHAIN_A_PATH="$BASE_IBC_PATH/$ANOMA_DIR/.anoma/$CHAIN_A_ID"
+
 printf "$STATUS_INFO Initialized Chain A: $CHAIN_A_ID\n\n"
+CHAIN_A_FAUCET=$( cat $BUILD_DIR/$ANOMA_DIR/.anoma/$CHAIN_A_ID/setup/other/wallet.toml | \
+  grep "faucet " |  cut -d \" -f2 )
+printf "$STATUS_INFO Setting Chain A faucet to $CHAIN_A_FAUCET\n\n"
 
-# TODO: Grab faucet address for Chain A
-
+# CHAIN B
 printf "$STATUS_INFO Initializing Chain B\n\n"
 CHAIN_B_INIT_STDOUT=$(./target/release/anomac utils init-network \
   --unsafe-dont-encrypt \
@@ -152,26 +160,69 @@ CHAIN_B_INIT_STDOUT=$(./target/release/anomac utils init-network \
   --wasm-checksums-path $WASM_CHECKSUMS_PATH)
 
 CHAIN_B_ID=$( echo "${CHAIN_B_INIT_STDOUT%?}" | grep "Derived" | sed 's/Derived chain ID: //g' )
-printf "$STATUS_INFO Initialized Chain B: $CHAIN_B_ID\n\n"
+CHAIN_A_PATH="$BASE_IBC_PATH/$ANOMA_DIR/.anoma/$CHAIN_A_ID"
 
-# TODO: Grab faucet address for Chain B
+printf "$STATUS_INFO Initialized Chain B: $CHAIN_B_ID\n\n"
+CHAIN_B_FAUCET=$( cat $BUILD_DIR/$ANOMA_DIR/.anoma/$CHAIN_B_ID/setup/other/wallet.toml | \
+  grep "faucet " |  cut -d \" -f2 )
+printf "$STATUS_INFO Setting Chain B faucet to $CHAIN_B_FAUCET\n\n"
+
+# Chain A - Copy wasms and checksums.json to appropriate directories
+
+cp wasm/*.wasm .anoma/$CHAIN_A_ID/wasm/
+cp wasm/checksums.json .anoma/$CHAIN_A_ID/wasm/
+cp wasm/*.wasm .anoma/$CHAIN_A_ID/setup/validator-0/.anoma/$CHAIN_A_ID/wasm/
+cp wasm/checksums.json .anoma/$CHAIN_A_ID/setup/validator-0/.anoma/$CHAIN_A_ID/wasm/
+
+printf "$STATUS_INFO Copied wasms and checkusms.json for $CHAIN_A_ID\n\n"
+
+# Chain B - Copy wasms and checksums.json to appropriate directories
+
+cp wasm/*.wasm .anoma/$CHAIN_B_ID/wasm/
+cp wasm/checksums.json .anoma/$CHAIN_B_ID/wasm/
+cp wasm/*.wasm .anoma/$CHAIN_B_ID/setup/validator-0/.anoma/$CHAIN_B_ID/wasm/
+cp wasm/checksums.json .anoma/$CHAIN_B_ID/setup/validator-0/.anoma/$CHAIN_B_ID/wasm/
+
+printf "$STATUS_INFO Copied wasms and checkusms.json for $CHAIN_B_ID\n\n"
 
 # Set up Hermes
 
-printf "$STATUS_INFO Installing Hermes\n\n"
-cd ../$HERMES_DIR && printf "\n$STATUS_WARN Changed directory to $(pwd)\n\n" && \
+printf "$STATUS_INFO Configuring Hermes\n\n"
+cd ../$HERMES_DIR && printf "$STATUS_WARN Changed directory to $(pwd)\n\n" && \
   git checkout $HERMES_BRANCH
 
+mkdir -p anoma_wasm
+printf "$STATUS_INFO Created directory $BUILD_DIR/$HERMES_DIR/anoma_wasm\n"
+mkdir -p anoma_wallet/$CHAIN_A_ID
+printf "$STATUS_INFO Created directory $BUILD_DIR/$HERMES_DIR/anoma_wallet/$CHAIN_A_ID\n"
+mkdir -p anoma_wallet/$CHAIN_B_ID
+printf "$STATUS_INFO Created directory $BUILD_DIR/$HERMES_DIR/anoma_wallet/$CHAIN_B_ID\n"
+
+# Copy chain files to Hermes
+
+cp $BUILD_DIR/$ANOMA_DIR/.anoma/$CHAIN_A_ID/setup/other/wallet.toml anoma_wallet/$CHAIN_A_ID
+printf "$STATUS_INFO Copied $BUILD_DIR/$ANOMA_DIR/.anoma/$CHAIN_A_ID/setup/other/wallet.toml -->\
+ $BUILD_DIR/$HERMES_DIR/anoma_wallet/$CHAIN_A_ID\n"
+
+cp $BUILD_DIR/$ANOMA_DIR/.anoma/$CHAIN_B_ID/setup/other/wallet.toml anoma_wallet/$CHAIN_B_ID
+printf "$STATUS_INFO Copied $BUILD_DIR/$ANOMA_DIR/.anoma/$CHAIN_B_ID/setup/other/wallet.toml -->\
+ $BUILD_DIR/$HERMES_DIR/anoma_wallet/$CHAIN_B_ID\n"
+
+cp $BUILD_DIR/$ANOMA_DIR/$WASM_CHECKSUMS_PATH anoma_wasm
+printf "$STATUS_INFO Copied $BUILD_DIR/$ANOMA_DIR/$WASM_CHECKSUMS_PATH -->\
+ $BUILD_DIR/$HERMES_DIR/anoma_wasm/\n"
+
+cp $BUILD_DIR/$ANOMA_DIR/wasm/tx_ibc*.wasm anoma_wasm
+printf "$STATUS_INFO Copied $BUILD_DIR/$ANOMA_DIR/wasm/tx_ibc*.wasm -->\
+ $BUILD_DIR/$HERMES_DIR/anoma_wasm/\n"
+
 # TODO: Copy configuration template to Hermes and add Namada Chain IDS
-# TODO: Copy wasms to anoma_wasm/
-# TODO: Copy each wallet to anoma_wallet/
 # TODO: Create connection and channel
 
-cd $BASE_IBC_PATH/$BUILD_DIR
+cd $BUILD_DIR && printf "\n$STATUS_WARN Changed directory to $(pwd)\n" 
 
-# TODO: Once chains are configured, generate a .env file for the Wallet UI:
-
-ENV_PATH=$BASE_IBC_PATH/$BASE_DIR/.env
+# Generate a .env file for the Wallet UI:
+ENV_PATH=$BUILD_DIR/.env
 
 write_env() {
   cat <<EOF > $ENV_PATH
